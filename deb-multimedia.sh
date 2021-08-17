4637e0e636742a151ae12631d8097e29863759e1
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-17 16:25:41 +0700

    Remove if statement in deb-multimedia.sh

--- deb-multimedia.sh
+++ deb-multimedia.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-if [ ! -f /etc/apt/sources.list.d/deb-multimedia.list ]; then
-    wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
-    dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
-    rm deb-multimedia-keyring_2016.8.1_all.deb
-fi
+wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
+dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
+rm deb-multimedia-keyring_2016.8.1_all.deb
