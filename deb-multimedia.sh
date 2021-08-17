e0d24534af133984d843417bb0dd9d49d8edb05e
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-17 16:20:57 +0700

    Install deb-multimedia key by using script

--- deb-multimedia.sh
+++ deb-multimedia.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ ! -f /etc/apt/sources.list.d/deb-multimedia.list ]; then
-    echo "deb http://www.deb-multimedia.org stretch main non free" >> /etc/apt/sources.list.d/deb-multimedia.list
     wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
     dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
     rm deb-multimedia-keyring_2016.8.1_all.deb
